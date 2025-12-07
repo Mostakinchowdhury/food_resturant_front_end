dcf7367c55eed4b1b638a02856b3cf080795a546
Mostakin chowdhury <chowdhurymostakin02@gmail.com>
2025-12-07 22:02:23 +0600

    final ultra pro max

--- app/components/Popular_r.tsx
+++ app/components/Popular_r.tsx
@@ -19,8 +19,8 @@ const Popular_r = async () => {
           >
             <div
               style={{
-                backgroundImage: item.buesness_logo
-                  ? `url(${process.env.ROOT?.concat(item.buesness_logo)})`
+                backgroundImage: item.buesness_logo_url
+                  ? `url(${process.env.ROOT?.concat(item.buesness_logo_url)})`
                   : `url('/img.png')`
               }}
               className="w-[122px] h-[156px] lg:w-[238px] lg:h-[266px] bg-no-repeat bg-center bg-cover"
