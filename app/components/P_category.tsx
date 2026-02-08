43ffe5394f56fd44ef3e951cb41b169a4c67ab99
Mostakin chowdhury <chowdhurymostakin02@gmail.com>
2026-02-08 21:58:16 +0600

    all

--- app/components/P_category.tsx
+++ app/components/P_category.tsx
@@ -1,10 +1,10 @@
-import { top6cattype } from '@/type/overview'
-import axios from 'axios'
-import { Route } from 'next'
-import Link from 'next/link'
+import { top6cattype } from '@/type/overview';
+import axios from 'axios';
+import { Route } from 'next';
+import Link from 'next/link';
 
 const P_category = async () => {
-  const result = await axios.get<top6cattype>(`${process.env.OVERVIEW}tc`)
+  const result = await axios.get<top6cattype>(`${process.env.OVERVIEW}tc`);
   return (
     <div className="space-y-6">
       {/* section title */}
@@ -13,27 +13,35 @@ const P_category = async () => {
       </h3>
       {/* foods container */}
       <section className="gridbox1">
-        {result.data.top_categories.map((item) => (
+        {result.data.top_categories.map(item => (
           <div key={item.id} className="rounded-2xl overflow-hidden">
             {/* food img */}
-            <Link href={('/menu?category=' + encodeURIComponent(item.name)) as Route}>
+            <Link
+              href={
+                ('/menu?category=' + encodeURIComponent(item.name)) as Route
+              }
+            >
               <div
                 style={{
-                  backgroundImage: `url('${item.image || '/img.png'}')`
+                  backgroundImage: `url('${item.image_url || '/img.png'}')`,
                 }}
                 className="bg-no-repeat bg-center bg-cover w-full h-[161px] lg:[203px]"
               />
             </Link>
             {/* food detail */}
             <div className="bg-txt2 lg:py-4 py-3 px-4 lg:px-5 flex flex-col items-start justify-center gap-1">
-              <h3 className="font-bold text-primary text-size0 md:text-size3">{item.name}</h3>
-              <h4 className="font-normal text-size0 text-white md:">{'Many shops'}</h4>
+              <h3 className="font-bold text-primary text-size0 md:text-size3">
+                {item.name}
+              </h3>
+              <h4 className="font-normal text-size0 text-white md:">
+                {'Many shops'}
+              </h4>
             </div>
           </div>
         ))}
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default P_category
+export default P_category;
--- app/type/overview.ts
+++ app/type/overview.ts
@@ -1,18 +1,19 @@
 export interface overview {
-  rider_count: number
-  order_count: number
-  partner_count: number
-  product_count: number
+  rider_count: number;
+  order_count: number;
+  partner_count: number;
+  product_count: number;
 }
 
 export interface topcategories {
-  id: number
-  name: string
-  description: string
-  image: string | null
-  total_quantity: number
+  id: number;
+  name: string;
+  description: string;
+  image: string | null;
+  total_quantity: number;
+  image_url: string;
 }
 
 export interface top6cattype {
-  top_categories: topcategories[]
+  top_categories: topcategories[];
 }
